5be54af7546606b9c1d7903f3ef17b50633101e6
houfeibi <86950568+houfeibi@users.noreply.github.com>
2023-07-28 16:47:36 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -5,7 +5,7 @@ const net = require('net');
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
 //let userID = '4bce352b-0e95-42eb-ad5b-0113b8c3bee8';
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '2c64d828-68ec-46d7-8e2a-21ab250baa3a';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
