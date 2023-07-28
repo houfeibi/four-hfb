7d83516b7b05d0e7f3b442c0134ba1046c8193e8
holyhbc <71000724+holyhbc@users.noreply.github.com>
2023-07-28 16:26:23 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,8 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = '4bce352b-0e95-42eb-ad5b-0113b8c3bee8';
+//let userID = '4bce352b-0e95-42eb-ad5b-0113b8c3bee8';
+let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
