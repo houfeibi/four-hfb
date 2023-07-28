db8cec89033287d502854b7545163f4d53852e81
holyhbc <71000724+holyhbc@users.noreply.github.com>
2023-07-28 16:26:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'4bce352b-0e95-42eb-ad5b-0113b8c3bee8').replace(/-/g, "");
+const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
